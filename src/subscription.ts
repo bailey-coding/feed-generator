5e72346bbf9b412fbc6f42aae03a8f140df9e26b
justbaileym <justbaileym@users.noreply.github.com>
2024-12-29 16:19:37 +0100

    Messing with a sentinment analysis feed generator

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,8 +1,22 @@
+import * as natural from 'natural'
+
 import {
   OutputSchema as RepoEvent,
   isCommit,
 } from './lexicon/types/com/atproto/sync/subscribeRepos'
 import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
+
+const Analyzer = natural.SentimentAnalyzer
+const stemmer = natural.PorterStemmer
+const analyzer = new Analyzer('English', stemmer, 'afinn')
+
+function interpretSentiment(score: number) {
+  if (score > 0.5) return 'Strongly Positive'
+  if (score > 0) return 'Positive'
+  if (score === 0) return 'Neutral'
+  if (score > -0.5) return 'Negative'
+  return 'Strongly Negative'
+}
 
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
   async handleEvent(evt: RepoEvent) {
@@ -13,17 +27,36 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     // This logs the text of every post off the firehose.
     // Just for fun :)
     // Delete before actually using
-    for (const post of ops.posts.creates) {
-      console.log(post.record.text)
-    }
+    // for (const post of ops.posts.creates) {
+    //   console.log(post.record.text)
+    // }
 
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = ops.posts.creates
       .filter((create) => {
-        // only alf-related posts
-        return create.record.text.toLowerCase().includes('alf')
+        // only queer-related posts
+        return (
+          create.record.text.toLowerCase().includes('queer') ||
+          create.record.text.toLowerCase().includes('trans')
+        )
+      })
+      .filter((create) => {
+        return create.record.langs?.indexOf('en') !== -1
+      })
+      .filter((create) => {
+        const result = analyzer.getSentiment(create.record.text.split(' '))
+        const humanReadable = interpretSentiment(result)
+        if (result > 0.5) {
+          console.log(`message: Score is ${result} - ${humanReadable}
+            post: ${create.record.text}\n`)
+        } else {
+          console.log(`Ignored message, result=${humanReadable}`)
+        }
+        return result > 0.5
       })
       .map((create) => {
+        // console.table(create)
+        // console.log(create.record.text)
         // map alf-related posts to a db row
         return {
           uri: create.uri,
